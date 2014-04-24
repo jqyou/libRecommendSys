33b8165cf65a8dace3028178d226f30f2accd6a6
why <why@why-tang>
2014-04-24 23:27:26 +0800

    search bookdetail

---
+++ src/com/servlet/bookDetail.java
@@ -0,0 +1,79 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.javabean.ConnectDB;
+
+/**
+ * Servlet implementation class bookDetail
+ */
+@WebServlet("/bookDetail")
+public class bookDetail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public bookDetail() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String bookid = request.getParameter("id");
+		String[] param = {bookid};
+		int[] which = {0};
+		String sqlString = "select * from libbook where bookid = ?";
+		try {
+			ConnectDB connectDB = new ConnectDB();
+			ResultSet record = connectDB.executeQuery(sqlString,param,which);
+		
+			if (record.next()){
+				JSONObject json = new JSONObject();  
+		        JSONArray array = new JSONArray();
+		        JSONObject member = new JSONObject();  
+		        
+		        member.put("id", record.getString("bookid"));
+		        member.put("name", record.getString("bookname"));
+	            member.put("author", record.getString("bookauthor"));
+	            member.put("type", record.getString("booktype"));
+	            array.add(member);  
+	            
+	            json.put("bookdetail", array);
+	            out.print(json.toString());
+			} else {
+				out.print("no");
+			}
+		} catch (SQLException e) {
+			out.print("error");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
